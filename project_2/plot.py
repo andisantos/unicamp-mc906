63b802eb5389b4990fc79598f45f2c05f137ef39
lcagustini <lucas.c.agustini@gmail.com>
2020-06-17 13:20:49 -0300

    Fixed plot x counting

--- project_2/plot.py
+++ project_2/plot.py
@@ -10,13 +10,13 @@ for directory in listdir('outputs/'):
     averages = np.loadtxt('outputs/'+directory+'/averages.txt', dtype=np.float, delimiter='\n')
     maxs = np.loadtxt('outputs/'+directory+'/maxs.txt', dtype=np.float, delimiter='\n')
     mins = np.loadtxt('outputs/'+directory+'/mins.txt', dtype=np.float, delimiter='\n')
-
+    size = len(maxs)
 
     fig, ax = plt.subplots()
-    ax.plot(range(df.max_generations), maxs, '-', color="#0294dd", label='Adequação máxima')
-    ax.plot(range(df.max_generations), averages, '-',color="#0e2795", label='Adequação média')
-    ax.plot(range(df.max_generations), mins, '-', color="#011254", label='Adequação mínima')
-    ax.fill_between(range(df.max_generations), mins, maxs, alpha=0.2)
+    ax.plot(range(size), maxs, '-', color="#0294dd", label='Adequação máxima')
+    ax.plot(range(size), averages, '-',color="#0e2795", label='Adequação média')
+    ax.plot(range(size), mins, '-', color="#011254", label='Adequação mínima')
+    ax.fill_between(range(size), mins, maxs, alpha=0.2)
     
     # Styling the graph
     ax.set_xlabel('Gerações')
