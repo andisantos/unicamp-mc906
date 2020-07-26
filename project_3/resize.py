19803804c9677e1de7bc9314ed12ee9f41ef8dac
Daniela <d166301@dac.unicamp.br>
2020-07-26 16:16:37 -0300

    Resized scrapy images

--- project_3/resize.py
+++ project_3/resize.py
@@ -1,24 +1,26 @@
 import os
 import cv2
+from math import floor, ceil
 
-root_folder = "./tcgdataset/dataset"
+folder_path = "./pokescrap/pokescrap/spiders/images/pngs"
+files_path = os.listdir(folder_path)
 
-folders_path = os.listdir(root_folder)
+target_size = (300, 300)
 
-target_size = (300, 238)
-standard_size = (240,330)
+for file_path in files_path:
+    file_full_path = os.path.join(folder_path, file_path)
 
-# percorre todas as pastas no root folder
-for folder in folders_path:
-
-    root_path = os.path.join(root_folder,folder)
-    files_path = os.listdir(root_path)
-    for file_path in files_path:
-        file_full_path = os.path.join(root_path, file_path)
-
-        img = cv2.imread(file_full_path, cv2.IMREAD_COLOR)
-        img_resized = cv2.resize(img, standard_size)
-        crop_img = img_resized[38:170, 26:214]
-        img_resized_final_v2_agora_vai = cv2.resize(crop_img, target_size)
-        img_border = cv2.copyMakeBorder(img_resized_final_v2_agora_vai, 31, 31, 0, 0, cv2.BORDER_CONSTANT, value=(255, 255, 255))
+    img = cv2.imread(file_full_path, cv2.IMREAD_COLOR)
+    height, width, _ = img.shape
+    if width > height:
+        ratio = 300 / width
+        img_resized = cv2.resize(img, (300, int(ratio * height)), interpolation=cv2.INTER_NEAREST)
+        img_border = cv2.copyMakeBorder(img_resized, floor((300-int(ratio*height))/2), ceil((300-int(ratio*height))/2),
+                                     0, 0, cv2.BORDER_CONSTANT, value=(255, 255, 255))
+        cv2.imwrite(file_full_path, img_border)
+    else:
+        ratio = 300 / height
+        img_resized = cv2.resize(img, (int(ratio * width), 300), interpolation=cv2.INTER_NEAREST)
+        img_border = cv2.copyMakeBorder(img_resized, 0, 0,  floor((300-int(ratio*width))/2), ceil((300-int(ratio*width))/2),
+                                         cv2.BORDER_CONSTANT, value=(255, 255, 255))
         cv2.imwrite(file_full_path, img_border)
