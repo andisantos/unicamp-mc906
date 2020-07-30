7341e3d849baea3cfc9fb17827023c14473681b3
lcagustini <lucas.c.agustini@gmail.com>
2020-07-30 15:10:30 -0300

    Full dataset of tcg pokemons

--- project_3/tcgdataset/tcgget.py
+++ project_3/tcgdataset/tcgget.py
@@ -3,13 +3,31 @@ from pokemontcgsdk import Card
 import os
 import requests
 
-os.mkdir("./dataset", 0o755);
-for i in range(1, 152):
+def get_type(cards): 
+    types = [card.types[0] for card in cards]
+
+    return max(set(types), key = types.count)
+
+#os.mkdir("./dataset", 0o755)
+for i in range(873, 894):
+    print("Getting cards for Pokemon " + str(i))
+
     cards = Card.where(nationalPokedexNumber=i, supertype='pokemon')
-    pokemon_path = "./dataset/"+str(i)
 
-    os.mkdir(pokemon_path, 0o755);
+    if (len(cards) == 0):
+        continue
 
-    for j, card in enumerate(cards):
+    pokemon_type = get_type(cards)
+    pokemon_path = "./dataset/"+pokemon_type
+
+    print("- Got "+cards[0].name+" of type "+pokemon_type)
+    print("- Got "+str(len(cards))+" cards for it, downloading images...")
+
+    try:
+        os.mkdir(pokemon_path, 0o755)
+    except:
+        pass
+
+    for card in cards:
         r = requests.get(card.image_url, allow_redirects=True)
-        open(pokemon_path+"/"+str(j)+".png", 'wb').write(r.content)
+        open(pokemon_path+"/"+card.id+".png", 'wb').write(r.content)
